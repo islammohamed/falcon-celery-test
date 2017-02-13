f025d7614ea8e563ca5e25ba8269d41736d30dec
Islam Mohamed <islammohamed@users.noreply.github.com>
2017-02-13 16:54:16 +0100

    Update tasks.py

--- resources/tasks.py
+++ resources/tasks.py
@@ -2,6 +2,4 @@ import celery
 app = celery.Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
 @app.task
 def hello():
-    from time import sleep
-    sleep(10)
     return 'hello world'
